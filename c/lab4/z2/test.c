8e5a056bea1410b8370a37042824fa72e242304d
devKica777-vm <devKica777@gmail.com>
2022-05-05 09:44:00 +0200

    Reformat data part2

--- c/lab4/z2/test.c
+++ c/lab4/z2/test.c
@@ -3,12 +3,45 @@
 #include <string.h>
 #include <time.h>
 
-int get_value(FILE *pft, int idx) {
-	fseek(pft, 0, SEEK_SET);
+int get_value(FILE *ptf_M, int idx) {
+	fseek(ptf_M, 0, SEEK_SET);
 	int content;
 	for (int i = 0; i <= idx; i++)
-		fscanf(pft, "%i", &content);
+		fscanf(ptf_M, "%i", &content);
 	return content;
+}
+
+int get_rows_number(FILE *ptf_f){
+	int length=0;
+    char* input = NULL;
+    size_t useless = 128;
+    int line_counter = 0;
+	while(1){
+        length = getline(&input, &useless, ptf_f);
+		if(length == -1)
+				break;
+		line_counter++;
+    }
+	return line_counter;
+}
+
+int get_columns_number(FILE *ptf_f){
+	fseek(ptf_f, 0, SEEK_SET);
+	int length=0;
+    char* input = NULL;
+    size_t useless = 128;
+    length = getline(&input, &useless, ptf_f);
+
+	int counter = 1;
+    for (int i = 0; i < length; i++)
+	{
+		if(input[i]==' ')
+		{
+			counter++;		
+		}
+	}
+
+	return counter;
 }
 
 int main(int arg_count, char **arg_values){
@@ -20,9 +53,9 @@ int main(int arg_count, char **arg_values){
 		return 1;
 	}
 	
-	FILE *ptf_A = fopen(arg_values[2], "r");
-	FILE *ptf_B = fopen(arg_values[3], "r");
-	FILE *ptf_C = fopen(arg_values[4], "w");
+	FILE *ptf_A = fopen(arg_values[1], "r");
+	FILE *ptf_B = fopen(arg_values[2], "r");
+	FILE *ptf_C = fopen(arg_values[3], "r");
 		
 	int rows_A;
 	int columns_A;
