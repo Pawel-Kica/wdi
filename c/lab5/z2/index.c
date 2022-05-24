d03dfd153fa8648a5fe647cb7cbe2c19a904d284
devKica777-vm <devKica777@gmail.com>
2022-05-24 14:46:44 +0200

    Add z2 to c/lab5 PART2

--- c/lab5/z2/index.c
+++ c/lab5/z2/index.c
@@ -17,11 +17,12 @@ void Term__init(struct Term *term_obj, int hour, int minute, int duration){
 struct Term* Term__create(int hour,int minute, int duration){
     struct Term *term_obj = malloc(sizeof(struct Term));
     Term__init(term_obj,hour,minute,duration);
+    return term_obj;
 }
 void Term__destroy(struct Term *term_obj){
     free(term_obj);
 }
-char* Term__toString(struct Term *term_obj){
+char *Term__toString(struct Term *term_obj){
     char *result = malloc(20*sizeof(*result));
 
     int counter = 0;
@@ -34,13 +35,33 @@ char* Term__toString(struct Term *term_obj){
         result[counter++] = hour;
     }
     else{
-        printf("\n Wchodzi wow \n");
         result[counter++] = hour/10;
-        printf("%i value \n",result[counter-1]);
         result[counter++] = hour%10;
-        printf("%i value \n",result[counter-1]);
     }
+    // printf("%i value \n",result[0]);
+    // printf("%i value \n",result[1]);
+    // printf("%s val\n",result);
+
     return result;
+}
+bool Term__earlierThan(struct Term *term1,struct Term *term2){
+    if(term1->hour==term2->hour){
+        if(term1->minute<term2->minute) return true;
+    }
+    return term1->hour<term2->hour;
+}
+bool Term__laterThan(struct Term *term1,struct Term *term2){
+    return !Term__earlierThan(term1,term2);
+}
+bool Term__equals(struct Term *term1,struct Term *term2){
+    return (term1->hour==term2->hour && term1->minute==term2->minute);
+}
+struct Term* Term__endTerm(struct Term *term1,struct Term *term2){
+    int duration = 0;
+    duration = duration + (term2->hour-term1->hour)*60;
+    duration = duration + term2->minute-term1->minute;
+
+    return Term__create(term1->hour,term1->minute,duration);
 }
 
 void run_units(){
@@ -80,10 +101,33 @@ void run_units(){
 
 
     // Term__toString
-    struct Term *test_obj_1_Term__toString = Term__create(val1,val2,val3);
-    CU_ASSERT_STRING_EQUAL(Term__toString(test_obj_1_Term__toString),"12");
+    struct Term *test_obj_1_Term__toString = Term__create(12,15,40);
+    CU_ASSERT_EQUAL(Term__toString(test_obj_1_Term__toString),"12");
+    
+    // Term_earlierThan
+    struct Term *test_obj_1_Term__earlierThan = Term__create(12,15,40);
+    struct Term *test_obj_2_Term__earlierThan = Term__create(12,20,40);
+    struct Term *test_obj_3_Term__earlierThan = Term__create(13,15,40);
+    struct Term *test_obj_4_Term__earlierThan = Term__create(13,15,40);
+    CU_ASSERT_TRUE(Term__earlierThan(test_obj_1_Term__earlierThan,test_obj_2_Term__earlierThan))
+    CU_ASSERT_TRUE(Term__earlierThan(test_obj_2_Term__earlierThan,test_obj_3_Term__earlierThan))
 
+    // Term_laterThan
+    CU_ASSERT_TRUE(Term__laterThan(test_obj_2_Term__earlierThan,test_obj_1_Term__earlierThan))
+    CU_ASSERT_TRUE(Term__laterThan(test_obj_3_Term__earlierThan,test_obj_2_Term__earlierThan))
 
+    // Term_equals
+    CU_ASSERT_TRUE(Term__equals(test_obj_3_Term__earlierThan,test_obj_4_Term__earlierThan))
+
+    // Term_endTerm
+
+    struct Term *test_obj_1_Term__endTerm = Term__create(9,45,90);
+    struct Term *test_obj_2_Term__endTerm = Term__create(10,15,90);
+    struct Term *result_endTerm = Term__endTerm(test_obj_1_Term__endTerm,test_obj_2_Term__endTerm);
+
+    CU_ASSERT_EQUAL(result_endTerm->hour,test_obj_1_Term__endTerm->hour);
+    CU_ASSERT_EQUAL(result_endTerm->minute,test_obj_1_Term__endTerm->minute);
+    CU_ASSERT_EQUAL(result_endTerm->duration,30);
 }
 int main()
 {
